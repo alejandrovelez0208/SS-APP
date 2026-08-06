35e2b99265670aadf023994c473d8a23aec6c2ab
alejandrovelez0208 <alejandrovelezarboleda0208@outlook.com>
2026-08-06 16:02:01 -0500

    Feat: changes

---
+++ Frontend/ss-app/src/app/signup/fields/sign-up.fields.ts
@@ -0,0 +1,27 @@
+export const SIGN_UP_FIELDS = [
+  {
+    key: 'profileType',
+    type: 'card-select',
+    options: [
+      {
+        value: 'escort',
+        title: 'Escort',
+        description: 'I am an escort looking for clients.',
+        img: '/woman.png',
+        class: 'type-escort'
+      },
+      {
+        value: 'member',
+        title: 'Member',
+        description: 'I am a member looking for escorts.',
+        img: '/man.png',
+        class: 'type-member'
+      }
+    ]
+  },
+  {
+    key: 'username',
+    type: 'text',
+    label: 'Username'
+  }
+];
